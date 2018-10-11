2c98e7dc9e0accca239939a62d03f1303f08a540
Grant Kayes <glk25@georgetown.edu>
2018-10-10 23:02:05 -0700

    fixed formatting

--- src/components/Locations.js
+++ src/components/Locations.js
@@ -2,34 +2,35 @@ import React from 'react';
 import Location from './Location'
 
 const Locations = (props) => {
-    const data = require('../locations.json')
-    const states = require('../states.json')
-    
-    return(
-        <div>
-            {states.map(state => {
-                let stateArray = []
-                data.forEach((data) => {
-                    if (data.state === state.abbreviation) {
-                        stateArray.push(<Location 
-                                            address={data.address}
-                                            state={data.state}
-                                            city={data.city}
-                                            allChecked={props.allChecked}
-                                        />)
-                    }
-                })
-                return (<div> 
-                            <h1> {state.name} </h1>
-                            <div>
-                                {stateArray.map( location =>
-                                    location
-                                )}
-                            </div>
-                        </div>)
-            })}
-        </div>
-    )
+  
+  const data = require('../locations.json')
+  const states = require('../states.json')
+
+  return (
+    <div>
+      {states.map(state => {
+        let stateArray = []
+        data.forEach((data) => {
+          if (data.state === state.abbreviation) {
+            stateArray.push(<Location
+              address={data.address}
+              state={data.state}
+              city={data.city}
+              allChecked={props.allChecked}
+            />)
+          }
+        })
+        return (<div>
+          <h1> {state.name} </h1>
+          <div>
+            {stateArray.map(location =>
+              location
+            )}
+          </div>
+        </div>)
+      })}
+    </div>
+  )
 }
 
 export default Locations
